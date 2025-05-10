7ffec12fa3424c2ddfeb8a78eceaee2330fccc38
Halil Çetin <halilcetin@Halil-MacBook-Air.local>
2025-05-10 13:51:53 +0300

    v4

--- src/Component/Callback.jsx
+++ src/Component/Callback.jsx
@@ -4,18 +4,21 @@ import axios from 'axios';
 
 function Callback() {
   const navigate = useNavigate();
-
+const basUrl="https://spotifylistappserver.onrender.com/api"
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const code = urlParams.get("code");
 console.log(code);
 
     if (code) {
-      axios.post("https://senin-api.com/api/spotify/token", { code })
+      const data =new FormData();
+      data.append("code",code);
+      //code
+      axios.post(`${basUrl}/Spotify/callback`, data)
         .then(res => {
           console.log("Token alındı:", res.data);
           // JWT token varsa localStorage'a kaydet
-          localStorage.setItem("spotify_access_token", res.data.accessToken);
+          localStorage.setItem("spotify_access_token", res.data.access_token);
           navigate("/playlist");
         })
         .catch(err => {
